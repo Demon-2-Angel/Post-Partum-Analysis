a0ce6de4b03c6130124b48d0d34775f589817bd7
Aniruddha Kumar <110346786+Demon-2-Angel@users.noreply.github.com>
2023-12-02 22:26:04 +0530

    removed lib

--- Streamlit_Post_Partum.py
+++ Streamlit_Post_Partum.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit.components.v1 as components 
 from streamlit_option_menu import option_menu
 
